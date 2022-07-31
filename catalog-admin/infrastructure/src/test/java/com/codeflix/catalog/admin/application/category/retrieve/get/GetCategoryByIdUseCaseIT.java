b20038e3c3f5273b1e7f1b7c368cec0be745ea46
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2022-07-31 18:03:14 -0400

    test: category integration tests by id.

---
+++ catalog-admin/infrastructure/src/test/java/com/codeflix/catalog/admin/application/category/retrieve/get/GetCategoryByIdUseCaseIT.java
@@ -0,0 +1,95 @@
+package com.codeflix.catalog.admin.application.category.retrieve.get;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import com.codeflix.catalog.admin.IntegrationTest;
+import com.codeflix.catalog.admin.domain.category.Category;
+import com.codeflix.catalog.admin.domain.category.CategoryGateway;
+import com.codeflix.catalog.admin.domain.category.CategoryID;
+import com.codeflix.catalog.admin.infrastructure.category.persistence.CategoryJpaEntity;
+import com.codeflix.catalog.admin.infrastructure.category.persistence.CategoryRepository;
+
+@IntegrationTest
+public class GetCategoryByIdUseCaseIT {
+
+    @Autowired
+    private DefaultGetCategoryByIdUseCase defaultGetCategoryByIdUseCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    void givenAValidId_whenCallsGetCategory_thenShouldReturnCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCategory = Category.newCategory(
+                expectedName, expectedDescription, expectedIsActive);
+        final var expectedId = aCategory.getId();
+
+        save(aCategory);
+
+        final var actualCategory = defaultGetCategoryByIdUseCase.execute(expectedId.getValue());
+
+        assertEquals(expectedId, actualCategory.id());
+        assertEquals(expectedName, actualCategory.name());
+        assertEquals(expectedDescription, actualCategory.description());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertEquals(aCategory.getCreatedAt().truncatedTo(ChronoUnit.MILLIS),
+                actualCategory.createdAt().truncatedTo(ChronoUnit.MILLIS));
+        assertEquals(aCategory.getUpdatedAt().truncatedTo(ChronoUnit.MILLIS),
+                actualCategory.updatedAt().truncatedTo(ChronoUnit.MILLIS));
+        assertEquals(aCategory.getDeletedAt(), actualCategory.deletedAt());
+    }
+
+    @Test
+    void givenAInvalidId_whenCallsGetCategory_thenShouldReturnNotFound() {
+        final var expectedId = CategoryID.from("123");
+        final var expectedErrorMessage = "Category with ID 123 was not found";
+
+        doThrow(new IllegalStateException(expectedErrorMessage))
+                .when(categoryGateway).findById(expectedId);
+
+        final var actualException = assertThrows(
+                IllegalStateException.class,
+                () -> defaultGetCategoryByIdUseCase.execute(expectedId.getValue()));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
+    @Test
+    void givenAValidId_whenGatewayThrowsException_thenShouldReturnException() {
+        final var expectedId = CategoryID.from("123");
+        final var expectedErrorMessage = "Gateway error";
+
+        doThrow(new IllegalStateException(expectedErrorMessage))
+                .when(categoryGateway).findById(expectedId);
+
+        final var actualException = assertThrows(
+                IllegalStateException.class,
+                () -> defaultGetCategoryByIdUseCase.execute(expectedId.getValue()));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
+    private void save(final Category... aCategory) {
+        categoryRepository.saveAllAndFlush(
+                Arrays.stream(aCategory)
+                        .map(CategoryJpaEntity::from)
+                        .toList());
+    }
+
+}
