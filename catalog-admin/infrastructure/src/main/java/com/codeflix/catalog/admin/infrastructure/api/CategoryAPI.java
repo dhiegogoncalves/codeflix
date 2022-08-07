41e85734690a0473b80c8360b81757cdc30501a1
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2022-08-07 17:43:32 -0400

    feat: including openapi springdoc library.

--- catalog-admin/infrastructure/src/main/java/com/codeflix/catalog/admin/infrastructure/api/CategoryAPI.java
+++ catalog-admin/infrastructure/src/main/java/com/codeflix/catalog/admin/infrastructure/api/CategoryAPI.java
@@ -9,13 +9,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.codeflix.catalog.admin.domain.pagination.Pagination;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RequestMapping(value = "categories")
+@Tag(name = "Categories")
 public interface CategoryAPI {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Category created"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     ResponseEntity<?> createCategory();
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "List all categories paginated")
     Pagination<?> listCategories(
             @RequestParam(name = "search", required = false, defaultValue = "") final String search,
             @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
